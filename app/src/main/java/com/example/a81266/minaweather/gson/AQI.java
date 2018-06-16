94949891aaca3d4b671a502715450167dbf94ac5
lmJerry <lmlovenana@hotmail.com>
2018-06-16 17:16:43 +0800

    显示天气信息

---
+++ app/src/main/java/com/example/a81266/minaweather/gson/AQI.java
@@ -0,0 +1,9 @@
+package com.example.a81266.minaweather.gson;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
--- app/src/main/java/com/example/a81266/minaweather/util/Utility.java
+++ app/src/main/java/com/example/a81266/minaweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.a81266.minaweather.db.City;
 import com.example.a81266.minaweather.db.County;
 import com.example.a81266.minaweather.db.Province;
+import com.example.a81266.minaweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -69,5 +71,16 @@ public class Utility {
             }
 
         return false;
+    }
+    public static Weather handleWeatherRespnse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
